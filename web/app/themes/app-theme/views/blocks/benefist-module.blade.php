735bb143df519d2a9e6fbc810da51cf521b7775d
obiwanbanan <Jt_xD@mail.ru>
2021-11-19 20:38:43 +0300

    benefist-module

--- web/app/themes/app-theme/views/blocks/benefist-module.blade.php
+++ web/app/themes/app-theme/views/blocks/benefist-module.blade.php
@@ -11,20 +11,30 @@
 @php
 	$headline = get_field('headline');
 	$benefists = get_field('benefist');
+
 @endphp
 <section>
 	<div class='container'>
-		<div class='benefist-wraper'>
+		<div class='benefist-wrapper'>
 			<div class='benefist-headline'>
-
 				{!! $headline !!}
 			</div>
-		</div>
-		<div class = 'benefist-content'>
-			@foreach($benefists as $benefist)
-				@php
-				@endphp
-			@endforeach
+
+			<div class='benefist-content-wrapper'>
+				@foreach($benefists as $benefist)
+					<div class='benefist-content'>
+						<div class='benefist-content-image-background'>
+							<div class='benefist-content-image'>
+								<img src="{!! $benefist['image']['url']!!}" alt="">
+							</div>
+						</div>
+						<div class='benefist-content-description'>
+							{!! $benefist['description'] !!}
+						</div>
+
+					</div>
+				@endforeach
+			</div>
 		</div>
 	</div>
 </section>
