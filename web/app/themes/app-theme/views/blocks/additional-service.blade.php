094c0ab0593b0769fe6b1a03afa1535cf40294d6
obiwanbanan <Jt_xD@mail.ru>
2021-11-20 11:12:30 +0300

    additional-service

---
+++ web/app/themes/app-theme/views/blocks/additional-service.blade.php
@@ -0,0 +1,42 @@
+@php
+	/*
+   * Title: Модуль дополнительный сервис
+   * Description: Модуль дополнительный сервис
+   * Category: Page Headers
+   * Icon: admin-comments
+   * Keywords: additional-service-module
+	 */
+
+@endphp
+
+@php
+	$headline = get_field('headline');
+	$content = get_field('content');
+@endphp
+
+<section>
+	<div class="container">
+		<div class="additional-service-background">
+			<div class="additional-service-headline">
+				{!! $headline !!}
+			</div>
+			<div class="additional-service-wrapper">
+				<div class="additional-service-content">
+					@foreach($content as $item)
+						<div class="additional-service-content-gradient">
+							<div class="additional-service-content-wrapper">
+								<div class="additional-service-content-headline-description">
+									{!! $item['headline_description'] !!}
+								</div>
+								<div class="additional-service-content-description">
+									{!! $item['description'] !!}
+								</div>
+							</div>
+						</div>
+
+					@endforeach
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
