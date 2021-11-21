e8e0f167cf72940fec52f311d2659f7e604d38aa
obiwanbanan <Jt_xD@mail.ru>
2021-11-21 16:30:33 +0300

    info - module

--- web/app/themes/app-theme/views/blocks/information-module.blade.php
+++ web/app/themes/app-theme/views/blocks/information-module.blade.php
@@ -6,49 +6,52 @@
 	 * Category: new
 	 */
 @endphp
-
 @php
 	$headline = get_field('headline');
 	$content = get_field('content');
 	$subheadline = get_field('subheadline');
 	$info_text = get_field('info_text');
 	$button = get_field('button');
-
-
 @endphp
-
 <section>
 	<div class="container">
 		<div class="information-module-background">
 			<div class="information-module">
-				<div class="information-module-headline">
-					{{$headline}}
-				</div>
+				@if(!empty($headline))
+					<div class="information-module-headline">
+						{{$headline}}
+					</div>
+				@endif
 				<div class="information-module-wrapper">
-
-
-					<div class="information-module-content">
-						@foreach($content as $key => $item)
-
-							<ul>
-								<li>
-								<span>{{$key+1}}.</span>
-									{{$item['content_text']}}
-								</li>
-							</ul>
-						@endforeach
-					</div>
-					<div class="information-module-subheadline">
-						{{$subheadline}}
-					</div>
-					<div class="information-module-info-text">
-						{{$info_text}}
-					</div>
-					<div class="information-module-button">
-						<button>
-							<a href="{{$button['url']}}">{{$button['title']}}</a>
-						</button>
-					</div>
+					@if(!empty($content))
+						<div class="information-module-content">
+							@foreach($content as $key => $item)
+								<ul>
+									<li>
+										<span>{{$key+1}}.</span>
+										{{$item['content_text']}}
+									</li>
+								</ul>
+							@endforeach
+						</div>
+					@endif
+					@if(!empty($subheadline))
+						<div class="information-module-subheadline">
+							{{$subheadline}}
+						</div>
+					@endif
+					@if(!empty($info_text))
+						<div class="information-module-info-text">
+							{{$info_text}}
+						</div>
+					@endif
+					@if(!empty($button))
+						<div class="information-module-button">
+							<button>
+								<a href="{{$button['url']}}">{{$button['title'] ?? 'Кнопка'}}</a>
+							</button>
+						</div>
+					@endif
 				</div>
 			</div>
 		</div>
